a1720141e50c0a980c80d40e407349f5d1e3b6fa
Arohan Dutt <arohandutt@gmail.com>
2020-06-21 19:23:46 -0400

    Remove default content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout.js"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage: React.FC = () => (
-  <Layout>
+  <React.Fragment>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Hi world</h1>
     <Link to="/photography">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </React.Fragment>
 )
 
 export default IndexPage
--- src/pages/photography/index.tsx
+++ src/pages/photography/index.tsx
@@ -1,18 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
 import "./photography.scss"
 
 const SecondPage: React.FC = () => (
-  <Layout>
+  <React.Fragment>
     <SEO title="Photography" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  </React.Fragment>
 )
 
 export default SecondPage
