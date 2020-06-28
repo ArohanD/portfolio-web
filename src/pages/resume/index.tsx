0a69e87adfa97b848af23b06a4d0bf623fcfd443
Arohan Dutt <arohandutt@gmail.com>
2020-06-28 01:17:38 -0400

    Render resume

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,14 +1,52 @@
 import React from "react"
-import resumeData from "./resume-data.json"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import "./resume-styles.scss"
 import SEO from "../../components/seo"
+import { LinkedInResume, Query } from "../../generated/graphql-types"
 
 const Resume: React.FC = () => {
+  const resumeQuery = useStaticQuery(graphql`
+    query ResumeQuery {
+      linkedInResume {
+        experiences {
+          companyName
+          title
+          description
+          location
+          startedOn
+          finishedOn
+        }
+      }
+    }
+  `) as Query
+
+  const { experiences } = resumeQuery.linkedInResume
+
   return (
     <React.Fragment>
       <SEO title="Resume" />
       <div className={"resume-page-wrapper"}>
-        <div className={"resume-content-wrapper"}>Resume</div>
+        <div className={"resume-content-wrapper"}>
+          {experiences.map(experience => {
+            if (experience.companyName !== "Personal Projects") {
+              return (
+                <React.Fragment key={experience.title}>
+                  <h4>{`${experience.title} @ ${experience.companyName}`}</h4>
+                  <p>{`${experience.startedOn.toUpperCase()} - ${
+                    experience.finishedOn
+                      ? experience.finishedOn.toUpperCase()
+                      : "present"
+                  }`}</p>
+                  <ul>
+                    {experience.description.split("•").map(bullet => {
+                      if (bullet !== "") return <li key={bullet}>{bullet}</li>
+                    })}
+                  </ul>
+                </React.Fragment>
+              )
+            }
+          })}
+        </div>
       </div>
     </React.Fragment>
   )
