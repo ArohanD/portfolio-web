612b40a916670616af056bdd561f198c9cc1f218
Arohan Dutt <arohandutt@gmail.com>
2020-06-28 13:44:05 -0400

    Add resume styling

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import "./resume-styles.scss"
 import SEO from "../../components/seo"
-import { LinkedInResume, Query } from "../../generated/graphql-types"
+import { Query } from "../../generated/graphql-types"
 
 const Resume: React.FC = () => {
   const resumeQuery = useStaticQuery(graphql`
@@ -26,23 +26,24 @@ const Resume: React.FC = () => {
     <React.Fragment>
       <SEO title="Resume" />
       <div className={"resume-page-wrapper"}>
+        <h1>Resume</h1>
         <div className={"resume-content-wrapper"}>
           {experiences.map(experience => {
             if (experience.companyName !== "Personal Projects") {
               return (
-                <React.Fragment key={experience.title}>
-                  <h4>{`${experience.title} @ ${experience.companyName}`}</h4>
+                <div key={experience.title} className="resume-experience-block">
+                  <h4 className="resume-experience-title">{`${experience.title} @ ${experience.companyName}`}</h4>
                   <p>{`${experience.startedOn.toUpperCase()} - ${
                     experience.finishedOn
                       ? experience.finishedOn.toUpperCase()
                       : "present"
                   }`}</p>
-                  <ul>
+                  <ul className="resume-experience-bullets">
                     {experience.description.split("•").map(bullet => {
                       if (bullet !== "") return <li key={bullet}>{bullet}</li>
                     })}
                   </ul>
-                </React.Fragment>
+                </div>
               )
             }
           })}
