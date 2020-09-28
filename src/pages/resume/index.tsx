0bf54e89f27a981dbc45546c6b5352c3867537b0
Arohan Dutt <arohandutt@gmail.com>
2020-09-27 23:35:00 -0400

    Add sidebar to resume

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -5,6 +5,7 @@ import SEO from "../../components/seo"
 import { Query } from "../../generated/graphql-types"
 import MobileNav from "../../components/mobileNav/"
 import { PageProps } from "gatsby"
+import SideBarLayout from "../../components/pageLayout"
 
 const Resume: React.FC = (props: PageProps) => {
   const resumeQuery = useStaticQuery(graphql`
@@ -26,31 +27,39 @@ const Resume: React.FC = (props: PageProps) => {
 
   return (
     <React.Fragment>
-      <SEO title="Resume Arohan Dutt" />
-      <div className={"resume-page-wrapper"}>
-        <h1>Resume</h1>
-        <div className={"resume-content-wrapper"}>
-          {nodes.map(experience => {
-            if (experience.Company_Name !== "Personal Projects") {
-              return (
-                <div key={experience.Title} className="resume-experience-block">
-                  <h4 className="resume-experience-title">{`${experience.Title} @ ${experience.Company_Name}`}</h4>
-                  <p>{`${experience.Started_On.toUpperCase()} - ${
-                    experience.Finished_On
-                      ? experience.Finished_On.toUpperCase()
-                      : "present"
-                  }`}</p>
-                  <ul className="resume-experience-bullets">
-                    {experience.Description.split("•").map(bullet => {
-                      if (bullet !== "") return <li key={bullet}>{bullet}</li>
-                    })}
-                  </ul>
-                </div>
-              )
-            }
-          })}
+      <SideBarLayout
+        title="Resume Arohan Dutt"
+        sideBarString={"resume"}
+        currentPath={location.pathname}
+      >
+        <div className={"resume-page-wrapper"}>
+          <h1>Resume</h1>
+          <div className={"resume-content-wrapper"}>
+            {nodes.map(experience => {
+              if (experience.Company_Name !== "Personal Projects") {
+                return (
+                  <div
+                    key={experience.Title}
+                    className="resume-experience-block"
+                  >
+                    <h4 className="resume-experience-title">{`${experience.Title} @ ${experience.Company_Name}`}</h4>
+                    <p>{`${experience.Started_On.toUpperCase()} - ${
+                      experience.Finished_On
+                        ? experience.Finished_On.toUpperCase()
+                        : "present"
+                    }`}</p>
+                    <ul className="resume-experience-bullets">
+                      {experience.Description.split("•").map(bullet => {
+                        if (bullet !== "") return <li key={bullet}>{bullet}</li>
+                      })}
+                    </ul>
+                  </div>
+                )
+              }
+            })}
+          </div>
         </div>
-      </div>
+      </SideBarLayout>
       <MobileNav URI={props.uri} />
     </React.Fragment>
   )
