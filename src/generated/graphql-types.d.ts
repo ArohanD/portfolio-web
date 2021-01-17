84c15768780f9b2a49250ada2fdb580abd99b187
Arohan Dutt <arohandutt@gmail.com>
2021-01-17 15:28:47 -0500

    Try lifted gatsby form

--- src/generated/graphql-types.d.ts
+++ src/generated/graphql-types.d.ts
@@ -4718,8 +4718,6 @@ export type QueryAllDirectoryArgs = {
 export type QuerySiteArgs = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
-  port?: Maybe<IntQueryOperatorInput>;
-  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
@@ -5255,8 +5253,6 @@ export type Site = Node & {
   __typename?: 'Site';
   buildTime?: Maybe<Scalars['Date']>;
   siteMetadata?: Maybe<SiteSiteMetadata>;
-  port?: Maybe<Scalars['Int']>;
-  host?: Maybe<Scalars['String']>;
   polyfill?: Maybe<Scalars['Boolean']>;
   pathPrefix?: Maybe<Scalars['String']>;
   id: Scalars['ID'];
@@ -5466,8 +5462,6 @@ export type SiteFieldsEnum =
   | 'siteMetadata___description'
   | 'siteMetadata___author'
   | 'siteMetadata___url'
-  | 'port'
-  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'id'
@@ -5560,8 +5554,6 @@ export type SiteFieldsEnum =
 export type SiteFilterInput = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
-  port?: Maybe<IntQueryOperatorInput>;
-  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -4,7 +4,34 @@ import MobileNav from "../../components/mobileNav/"
 import { PageProps } from "gatsby"
 import SideBarLayout from "../../components/pageLayout"
 
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
 const Contact: React.FC<any> = (props: PageProps) => {
+  const [state, setState] = React.useState({})
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
   return (
     <React.Fragment>
       <SideBarLayout
@@ -13,24 +40,30 @@ const Contact: React.FC<any> = (props: PageProps) => {
         currentPath={props.uri}
       >
         <div className="contact-container">
-          <form name="contact" method="POST" data-netlify="true" action='/contact/success'>
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            action="/contact/success/"
+            onSubmit={handleSubmit}
+          >
             <input type="hidden" name="form-name" value="contact" />
             <p>
               <label>
                 <p>Name</p>
-                <input type="text" name="name" />
+                <input type="text" name="name" onChange={handleChange} />
               </label>
             </p>
             <p>
               <label>
                 <p>Email</p>
-                <input type="email" name="email" />
+                <input type="email" name="email" onChange={handleChange} />
               </label>
             </p>
             <p>
               <label>
                 <p>Message</p>
-                <textarea name="message"></textarea>
+                <textarea name="message" onChange={handleChange} />
               </label>
             </p>
             <p>
